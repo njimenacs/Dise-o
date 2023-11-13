43d7c9ca101f1253d0afe70210592bebd4bfdbaf
AndressBernal <bernal20yl@gmail.com>
2023-11-13 13:54:37 -0500

    creacion de variables: identificacion, nombre, telefono, email, clave

--- Diseño/Persona.cs
+++ Diseño/Persona.cs
@@ -8,5 +8,16 @@ namespace Diseño
 {
     internal class Persona
     {
+        int Identificacion;
+        string Nombre;
+        int Telefono;
+        string email;
+        int clave;
+
+        public int Identificacion1 { get => Identificacion; set => Identificacion = value; }
+        public string Nombre1 { get => Nombre; set => Nombre = value; }
+        public int Telefono1 { get => Telefono; set => Telefono = value; }
+        public string Email { get => email; set => email = value; }
+        public int Clave { get => clave; set => clave = value; }
     }
 }
