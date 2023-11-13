9b92ac0a70b1cf1b9558f407c1078b46c7b5f668
BrayanSoftw <147113423+BrayanSoftw@users.noreply.github.com>
2023-11-13 18:20:49 -0500

    Update Cliente.cs

--- Diseño/Cliente.cs
+++ Diseño/Cliente.cs
@@ -8,5 +8,52 @@ namespace Diseño
 {
     internal class Cliente
     {
+            public class Cliente : Persona
+    {
+        public string Cedula { get; set; }
+        public string Direccion { get; set; }
+
+        public class Parqueo
+        {
+            public string Placa { get; set; }
+            public DateTime FechaHoraIngreso { get; set; }
+            public DateTime FechaHoraSalida { get; set; }
+            public string CodigoDeBarras { get; set; }
+
+            public void Ingresar(string placa)
+            {
+                this.Placa = placa;
+                this.FechaHoraIngreso = DateTime.Now;
+            }
+
+            public void Salir()
+            {
+                this.FechaHoraSalida = DateTime.Now;
+            }
+
+            public void ImprimirTicket()
+            {
+                // Imprimir ticket
+            }
+
+            public void ImprimirRecibo()
+            {
+                // Imprimir recibo
+            }
+
+            public double CalcularTiempo()
+            {
+                // Calcular tiempo de estacionamiento
+                return (this.FechaHoraSalida - this.FechaHoraIngreso).TotalHours;
+            }
+
+            public double CalcularValorTotal()
+            {
+                // Calcular valor total del estacionamiento
+                return this.CalcularTiempo() * 2000;
+            }
+        }
     }
 }
+    }
+
